fce39c5c0077afa3d7ac2eea522b9dd9e8073f84
Rich <richgrant192@gmail.com>
2020-03-29 13:32:08 +0100

    updating episode 3 comment with more detail

--- whodunnit.js
+++ whodunnit.js
@@ -51,23 +51,9 @@ const secondVerdict = `The murderer is ${murderer}.`;
 console.log('Second Verdict: ', secondVerdict);
 
 // This will return two verdicts: the first being Mrs. Peacock, then Professor Plum.
-// Reason: Mrs. Peacock's verdict is returned first, using the declareMurderer() function, which was defined in block scope using const.
-// Professor Plum's verdict is returned second, using a const assignment (secondVerdict) which references the initial let variable defined in the first line.
+// Reason: Mrs. Peacock's verdict is returned first, using the declareMurderer() function, which was defined in block scope using const. The murderer variable was reassigned to Mrs. Peacock in this block also, but this is fine as it was done with let, which can be reassigned.
+// Professor Plum's verdict is returned second, using a const assignment (secondVerdict) which references the initial let variable defined in the first line. The reason this doesn't return Mrs. Peacock is because her name was defined with let within block scope, which means it can't be accessed outside of that block. Professor Plum was assigned with let outside of a block, so his name can be called again from the first line let variable.
 
-// Episode 4
-
-let suspectOne = 'Miss Scarlet';
-let suspectTwo = 'Professor Plum';
-let suspectThree = 'Mrs. Peacock';
-
-const declareAllSuspects = function() {
-  let suspectThree = 'Colonel Mustard';
-  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-}
-
-const suspects = declareAllSuspects();
-console.log(suspects);
-console.log(`Suspect three is ${suspectThree}.`);
 
 
 
