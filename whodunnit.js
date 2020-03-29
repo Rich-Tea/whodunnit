7b00c4dafd6fd8c1ec897973a0d63874026a2a6b
Rich <richgrant192@gmail.com>
2020-03-29 13:39:25 +0100

    added episode 4

--- whodunnit.js
+++ whodunnit.js
@@ -54,6 +54,20 @@ console.log('Second Verdict: ', secondVerdict);
 // Reason: Mrs. Peacock's verdict is returned first, using the declareMurderer() function, which was defined in block scope using const. The murderer variable was reassigned to Mrs. Peacock in this block also, but this is fine as it was done with let, which can be reassigned.
 // Professor Plum's verdict is returned second, using a const assignment (secondVerdict) which references the initial let variable defined in the first line. The reason this doesn't return Mrs. Peacock is because her name was defined with let within block scope, which means it can't be accessed outside of that block. Professor Plum was assigned with let outside of a block, so his name can be called again from the first line let variable.
 
+// Episode 4
 
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
 
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
 
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+
+// First this will return: Miss Scarlet, Professor Plum, and Colonel Mustard. This is returned with the block within the first declareAllSuspects function call, where suspectThree is reassigned to Colonel Mustard. This reassignment is possible because it uses let and is performed with the block.
+// Secondly it will return: only Mrs. Peacock, as we are logging suspectThree to the console - suspectThree in this case is the initial let variable defined on line 61. The reason it doesn't return Colonel Mustard as suspectThree is because his reassignment was performed with let within a block (lines 63-66), and let variables within blocks are not accessible outside of that block. Mrs. Peacock was defined with let also, but not inside a block, so Mrs. Peacock as suspectThree is accessible and can be called again.
